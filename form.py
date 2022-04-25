4a5bb50bf632cb85799e9c03354ebf35a06a0fb4
Oksana19761219 <o.valioniene@gmail.com>
2022-04-25 21:48:19 +0300

    deleted files

--- form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FloatField, IntegerField
-from wtforms.validators import DataRequired
-
-class ProductForm(FlaskForm):
-    name = StringField('name', [DataRequired()])
-    price = FloatField('price', [DataRequired()])
-    quantity = IntegerField('quantity', [DataRequired()])
-    submit = SubmitField('Submit')
